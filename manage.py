fc27f14855b8320d4348647a67ca7c020480559f
zhaomingyan <964187786@qq.com>
2018-04-14 20:42:55 +0800

    mysql数据库的集成

--- manage.py
+++ manage.py
@@ -1,13 +1,24 @@
 # coding=utf-8
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+
+class Config(object):
+    """工程配置信息"""
+    DEBUG = True
+    # 数据库的配置信息
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/ihome"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+app.config.from_object(Config)
+db = SQLAlchemy(app)
 
 
 @app.route('/')
 def index():
-    return index
+    return 'index'
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
