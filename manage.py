7f5319d759a0076e5fd1ed3e342d2412e17f19a3
zhaomingyan <964187786@qq.com>
2018-04-14 21:01:27 +0800

    redis数据库的集成

--- manage.py
+++ manage.py
@@ -1,6 +1,9 @@
 # coding=utf-8
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import redis
+
+
 app = Flask(__name__)
 
 class Config(object):
@@ -9,10 +12,14 @@ class Config(object):
     # 数据库的配置信息
     SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/ihome"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    #设置redis
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
 
 
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+redis_store = redis.StrictRedis(host=Config.REDIS_HOST,port=Config.REDIS_PORT)
 
 
 @app.route('/')
